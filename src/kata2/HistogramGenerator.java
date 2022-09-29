358862cc74d485d49b48a7e7bf858f09ade1503c
Entrar <Entrar@10.22.147.22>
2022-09-29 11:35:52 +0100

    v4

--- src/kata2/HistogramGenerator.java
+++ src/kata2/HistogramGenerator.java
@@ -3,17 +3,17 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HistogramGenerator {
-    int[] data;
+public class HistogramGenerator<T> {
+    T[] data;
 
-    public HistogramGenerator(int[] data) {
+    public HistogramGenerator(T[] data) {
         this.data = data;
         
     }
 
-    public Map<Integer, Integer> getHistogram() {
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        for (int key : this.data) {
+    public Map<T, Integer> getHistogram() {
+        Map<T, Integer> histogram = new HashMap<T, Integer>();
+        for (T key : this.data) {
             histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
         
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int [] data = {1, 6, 2, 8, 3, 8, 2, 7, 3, 4, 2, 9, 0, 6};
+        Integer [] data = {1, 6, 2, 8, 3, 8, 2, 7, 3, 4, 2, 9, 0, 6};
         
         HistogramGenerator histogramGenerator = new HistogramGenerator(data);
         Map<Integer, Integer> histogram = histogramGenerator.getHistogram();
